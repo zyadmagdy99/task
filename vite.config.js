64c674b3dbbe17a47e21e5ca3025077c751b0698
zyad magdy <zyadmagdy08@gmail.com>
2025-09-08 15:44:33 +0300

    last update2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import rtl from 'tailwindcss-rtl'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
         tailwindcss(),
-        rtl(),
+        
 
   ],
 })
